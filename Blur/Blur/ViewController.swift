d670a76ffdc074c733d5e832535bef67a3f4a881
Xavi Moll <xmollv@gmail.com>
2019-01-01 18:58:14 +0100

    Added the base project

---
+++ Blur/Blur/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Blur
+//
+//  Created by Xavi Moll on 01/01/2019.
+//  Copyright © 2019 xmollv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
